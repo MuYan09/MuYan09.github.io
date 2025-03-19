1bd160b8ccd379ed290d869bce8f49858aa0995b
muyan <rain97@163.com>
2025-03-19 23:47:50 +0800

    Site updated:YYYY-03-2025年3月19日 23:47:49

--- js/random.js
+++ js/random.js
@@ -1,1 +1,1 @@
-var posts=["archives/c920ed04.html","archives/d2f6e5e8.html","archives/Hexo-Github-Vercel-DeploymentTutorial.html","archives/12986.html","archives/hexo-Butterfly-bqwg.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["archives/hexo-Butterfly-bqwg.html","archives/12986.html","archives/c920ed04.html","archives/d2f6e5e8.html","archives/Hexo-Github-Vercel-DeploymentTutorial.html"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
